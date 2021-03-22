e3243706f7caa562d8a98df6c8fb175aa33abaa1
dro-1 <lorddro1532@gmail.com>
2021-03-22 21:31:58 +0100

    Project Init

---
+++ lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Expense Tracker',
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Flutter App'),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Card(
+            child: Text('My Chart'),
+          ),
+          Card(
+            child: Text('My Transactions'),
+          ),
+        ],
+      ),
+    );
+  }
+}
